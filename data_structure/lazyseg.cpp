e55a46331990ddb25cc97bdb5fd5419a05fde862
Shun Kida <creative.spider.2013@gmail.com>
2020-01-13 22:32:44 +0900

    Add lazy segtree

---
+++ data_structure/lazyseg.cpp
@@ -0,0 +1,84 @@
+//-------------------------------------------------
+//--Lazy Evaluation Segment tree (Prototype)
+//-------------------------------------------------
+template<typename T, typename E>
+class LazySegmentTree
+{
+private:
+    using F = ::std::function<T(T,T)>;
+    using G = ::std::function<T(T,E)>;
+    using H = ::std::function<E(E,E)>;
+    F f; G g; H h;
+    int n, height;
+    ::std::vector<T> seg;
+    ::std::vector<E> lazy;
+    T te; E ee;
+public:
+    LazySegmentTree(int _n, F f, G g, H h, T te, E ee)
+        : f(f),g(g),h(h),te(te),ee(ee)
+    {
+        init(_n);
+    }
+    LazySegmentTree(::std::vector<T> &v, F f, G g, H h, T te, E ee)
+        : f(f),g(g),h(h),te(te),ee(ee)
+    {
+        int _n = v.size();
+        init(_n);
+        for(int i=n; i<n+_n; i++) seg[i] = v[i-n];
+        build();
+    }
+    void init(int _n){
+        n = 1; height=0;
+        while(n<_n) n<<=1, height++;
+        seg.resize(2*n, te);
+        lazy.resize(2*n, ee);
+    }
+    void build(){
+        for(int i=n-1; i>0; i--) seg[i] = f(seg[i<<1], seg[i<<1|1]);
+    }
+    T eval(int k){
+        return (lazy[k]==ee) ? seg[k] : g(seg[k],lazy[k]);
+    }
+    void waveto(int k){
+        for(int i=height; i>0; i--) wave(k>>i);
+    }
+    void wave(int k){
+        seg[k] = eval(k);
+        lazy[k<<1] = h(lazy[k<<1],lazy[k]);
+        lazy[k<<1|1] = h(lazy[k<<1|1],lazy[k]);
+        lazy[k] = ee;
+    }
+    void recalc(int k){
+        for(int i=1; i<=height; i++){
+            int m = k>>i;
+            seg[m] = f(eval(m<<1), eval(m<<1|1));
+        }
+    }
+    void update(int a, int b, E x){
+        if (b<=a) return;
+        waveto(a+=n);
+        waveto(b+=n-1);
+        T L=ee, R=ee;
+        int l=a, r=b;
+        while(l<=r){
+            if (l&1) lazy[l] = h(lazy[l], x), l++;
+            if (!(r&1)) lazy[r] = h(lazy[r], x), r--;
+            l>>=1; r>>=1;
+        }
+        recalc(a);
+        recalc(b);
+    }
+    T query(int a, int b){
+        waveto(a+=n);
+        waveto(b+=n-1);
+        T L=te, R=te;
+        int l=a, r=b;
+        while(l<=r){
+            if (l&1) L = f(L, eval(l++));
+            if (!(r&1)) R = f(eval(r--), R);
+            l>>=1; r>>=1;
+        }
+        return f(L,R);
+    }
+    T operator[](int k){return query(k, k+1);};
+};
