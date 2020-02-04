1d6fb746c3928a6e52215b2f7f65c7aa538cfa56
Shun Kida <creative.spider.2013@gmail.com>
2020-02-05 00:24:27 +0900

    template update

--- template.cpp
+++ template.cpp
@@ -7,6 +7,9 @@
 #define RITR(x,c) for(__typeof(c.rbegin()) x=c.rbegin();x!=c.rend();x++)
 #define setp(n) fixed << setprecision(n)
 
+template<class T> bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T> bool chmin(T &a, const T &b) { if (a>b) { a=b; return 1; } return 0; }
+
 #define ld long double
 #define ll long long
 #define vll vector<ll>
