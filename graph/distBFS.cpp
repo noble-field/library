df09f64d8ce45e372f384e658f416e486b54f2a3
Shun Kida <creative.spider.2013@gmail.com>
2021-05-18 21:58:09 +0900

    fix 01BFS

--- graph/distBFS.cpp
+++ graph/distBFS.cpp
@@ -18,17 +18,18 @@ vector<int> distBFS(Graph<E> &G, int s)
 }
 
 template<class E>
-vector<int> dist01BFS(Graph<E> &G, int s)
+vector<int> dist01BFS(Graph<E> &G, int s, int inf=1e9)
 {
-    vector<int> dist(G.size(),-1); dist[s]=0;
+    vector<int> dist(G.size(),inf); dist[s]=0;
     deque<int> deq; deq.push_back(s);
     while(!deq.empty()){
         int v = deq.front(); deq.pop_front();
         for(auto e:G[v]){
-            if (dist[e.to]!=-1) continue;
-            dist[e.to]=dist[v]+e.cost;
-            if (e.cost>0) deq.push_back(e.to);
-            else deq.push_front(e.to);
+            if (dist[e.to]>dist[v]+e.cost){
+                dist[e.to]=dist[v]+e.cost;
+                if (e.cost>0) deq.push_back(e.to);
+                else deq.push_front(e.to);
+            }
         }
     }
     return dist;
