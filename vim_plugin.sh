b8e790a890453b2a0485de546d94ded1e5000932
spider5415 <creative.spider.2013@gmail.com>
2021-08-30 12:27:46 +0900

    fix vim_plugin.sh
    fix vim_plugin.sh
    
    fix vim_plugin.sh

--- vim_plugin.sh
+++ vim_plugin.sh
@@ -11,13 +11,13 @@ cd $PLUGIN_PATH
 [ ! -d "${PLUGIN_PATH}/vim-airline-themes" ] && git clone https://github.com/vim-airline/vim-airline-themes.git
 
 # vim-fugitive
-[ ! -d "${PLUGIN_PATH}/vim-fugitive" ] && git clone https://tpope.io/vim/fugitive.git
+[ ! -d "${PLUGIN_PATH}/fugitive" ] && git clone https://tpope.io/vim/fugitive.git
 
 # vim-gitgutter
 [ ! -d "${PLUGIN_PATH}/vim-gitgutter" ] && git clone https://github.com/airblade/vim-gitgutter.git
 
 # fzf.vim
-if [ ! -d "~/.fzf" ] then;
+if [ ! -d "~/.fzf" ]; then
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 	~/.fzf/install
 fi
@@ -25,7 +25,7 @@ fi
 [ ! -d "${PLUGIN_PATH}/fzf.vim" ] && git clone https://github.com/junegunn/fzf.vim.git
 
 # coc.nvim
-if [ ! -d  "${PLUGIN_PATH}/coc.nvim" ] then;
+if [ ! -d  "${PLUGIN_PATH}/coc.nvim" ]; then
 	git clone https://github.com/neoclide/coc.nvim.git
 	echo "To install nodejs, run\'curl -sL install-node.now.sh/lts | bash\'"
 	echo "To complete coc.nvim, run\'yarn install --frozen-lockfile\'"
